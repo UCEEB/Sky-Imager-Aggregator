ad7e5e975274a04e5e9ad2bd0199f3f5b528b901
Azim Mazinani <mazinani@tutanota.com>
2019-08-14 11:29:04 +0200

    Creating test

---
+++ test/test_SIAUtils.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+
+class TestSIAUtils(TestCase):
+    def test_load_image(self):
+        self.fail()
+
+    def test_apply_mask(self):
+        self.fail()
+
+    def test_apply_custom_processing(self):
+        self.fail()
+
+    def test_encrypt_message(self):
+        self.fail()
+
+    def test_send_post_request(self):
+        self.fail()
+
+    def test_upload_json(self):
+        self.fail()
+
+    def test_upload_bson(self):
+        self.fail()
+
+    def test_get_sunrise_and_sunset_date(self):
+        self.fail()
+
+    def test_get_path_to_storage(self):
+        self.fail()
+
+    def test_save_to_storage(self):
+        self.fail()
+
+    def test_get_free_space_storage(self):
+        self.fail()
+
+    def test_save_irradiance_csv(self):
+        self.fail()
+
+    def test_test_internet_connection(self):
+        self.fail()
