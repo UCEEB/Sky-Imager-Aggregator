e068bb133a384809098324c8a9de7db40ec0041c
Azim Mazinani <mazinani@tutanota.com>
2019-09-10 10:54:28 +0200

    Adding basic methods to Camera class

--- SkyImageAgg/Collector.py
+++ SkyImageAgg/Collector.py
@@ -1,9 +1,31 @@
-
-class IrrSensor:
-    def __init__(self):
-        pass
+from picamera import PiCamera
+import time
 
 
 class Camera:
     def __init__(self, integrated=True):
         self.integrated = integrated
+        if self.integrated:
+            self.cam = PiCamera()
+
+    def set_resolution(self, width, height):
+        self.cam.resolution = (width, height)
+
+    def start_preview(self):
+        self.cam.start_preview()
+
+    def stop_preview(self):
+        self.cam.stop_preview()
+
+    # TO DO
+    def snap(self, image):
+        self.cam.capture(image)
+
+    def cap_video(self):
+        pass
+
+
+class IrrSensor:
+    def __init__(self):
+        pass
+
