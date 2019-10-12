dee6eece284bd3c808c79721a5c72a6d6454e05f
Azim Mazinani <mazinani@tutanota.com>
2019-10-12 20:56:42 +0200

    Removing all twilight-related methods and moving them to the new class in Controller.py called TimeManager.

--- SkyImageAgg/SkyImager.py
+++ SkyImageAgg/SkyImager.py
@@ -16,15 +16,14 @@ from SkyImageAgg.Controller import Controller
 from SkyImageAgg.GSM import Messenger, GPRS, retry_on_failure
 
 _parent_dir_ = os.path.dirname(os.path.dirname(__file__))
-_twilight_coll_ = os.path.join(_parent_dir_, 'ann_twilight_coll.pkl')
 
 
 def loop_infinitely(time_gap=3):
     def deco_retry(f):
         def f_retry(*args, **kwargs):
             while True:
+                kick_off = time.time()
                 if time_gap:
-                    kick_off = time.time()
                     f(*args, **kwargs)
                     try:
                         wait = time_gap - (time.time() - kick_off)
@@ -45,6 +44,9 @@ class SkyScanner(Controller, Configuration):
         super().__init__(
             server=self.config.server,
             camera_id=self.config.id,
+            camera_latitude=self.config.camera_latitude,
+            camera_longitude=self.config.camera_longitude,
+            camera_altitude=self.config.camera_altitude,
             image_quality=self.config.image_quality,
             auth_key=self.config.key,
             storage_path=self.config.storage_path,
@@ -56,7 +58,7 @@ class SkyScanner(Controller, Configuration):
             pwd=self.config.cam_pwd,
             rpi_cam=self.config.integrated_cam,
             log_dir=self.config.log_path,
-            log_stream=True
+            log_stream=self.config.log_to_console
         )
         try:
             if self.config.light_sensor:
@@ -77,27 +79,27 @@ class SkyScanner(Controller, Configuration):
         self.upload_stack = LifoQueue()
         self.write_stack = LifoQueue()
         self.day_no = dt.datetime.utcnow().timetuple().tm_yday
-        self.sunrise, self.sunset = self.get_today_twilight_times()
+        self.sunrise, self.sunset = self.get_today_twilight_times(day_no=self.day_no)
         self.daytime = False
 
     # TODO
     def check_requirements(self):
         if not os.path.exists(_twilight_coll_):
             try:
-                self.collect_annual_twilight_times()
+                self.collect_annual_twilight_times(dir_path=_parent_dir_)
             except Exception as e:
                 self.logger.exception(e)
 
         if os.path.exists(_twilight_coll_):
-            self.logger.info('twilight times are already collected and stored at {}'.format(_twilight_coll_))
+            self.logger.info('twilight times are already collected')
 
             with open(_twilight_coll_, 'rb') as handle:
                 col = pickle.load(handle)
 
                 if col['geo_loc'] != (self.config.camera_latitude, self.config.camera_longitude):
-                    self.logger.info('it seems your location has changed. Collecting new twilight data...')
+                    self.logger.info('your location has changed. Collecting new twilight data...')
                     try:
-                        self.collect_annual_twilight_times()
+                        self.collect_annual_twilight_times(_parent_dir_)
                     except Exception as e:
                         self.logger.exception(e)
 
@@ -107,59 +109,6 @@ class SkyScanner(Controller, Configuration):
                 self.Messenger.send_sms(self.GSM_phone_no, 'SOME MESSAGE AND INFO')
             except Exception as e:
                 self.logger.exception(e)
-
-    def sync_time(self):
-        self.logger.info('synchronizing the time with {}'.format(self.config.ntp_server))
-        if os.system('sudo /usr/sbin/ntpd {}'.format(self.config.ntp_server)) == 0:
-            return True
-
-    def get_sunrise_and_sunset_time(self, date=None):
-        if not date:
-            date = dt.datetime.now(dt.timezone.utc).date()
-
-        astral = Astral()
-        astral.solar_depression = 'civil'
-        location = Location((
-            'custom',
-            'region',
-            self.config.camera_latitude,
-            self.config.camera_longitude,
-            'UTC',
-            self.config.camera_altitude
-        ))
-        sun = location.sun(date=date)
-
-        return sun['sunrise'].time(), sun['sunset'].time()
-
-    def collect_annual_twilight_times(self):
-        self.logger.info('collecting the annual twilight times based on your given location')
-        collection = {
-            'geo_loc': (self.config.camera_latitude,
-                        self.config.camera_longitude)
-        }
-
-        dates = np.arange(
-            # 2021 is chosen as it's a leap year with 366 days
-            dt.datetime(2020, 1, 1),
-            dt.datetime(2021, 1, 1),
-            dt.timedelta(days=1)
-        ).astype(dt.datetime).tolist()
-
-        for date in dates:
-            collection[date.timetuple().tm_yday] = self.get_sunrise_and_sunset_time(date=date)
-
-        with open(_twilight_coll_, 'wb') as file:
-            pickle.dump(collection, file, protocol=pickle.HIGHEST_PROTOCOL)
-
-        return collection
-
-    def get_today_twilight_times(self):
-        with open(_twilight_coll_, 'rb') as handle:
-            col = pickle.load(handle)
-        return col[self.day_no]
-
-    def _stamp_curr_time(self):
-        return dt.datetime.utcnow().strftime(self.time_format)
 
     def scan(self):
         # store the current time according to the time format
@@ -262,7 +211,7 @@ class SkyScanner(Controller, Configuration):
             if curr_time.timetuple().tm_yday != self.day_no:  # check if the day has changed
                 self.day_no = curr_time.timetuple().tm_yday
                 try:
-                    self.sunrise, self.sunset = self.get_today_twilight_times()
+                    self.sunrise, self.sunset = self.get_today_twilight_times(day_no=self.day_no)
                 except Exception as e:
                     self.logger.exception(e)
 
