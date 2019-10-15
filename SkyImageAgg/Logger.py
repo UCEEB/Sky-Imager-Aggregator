452b0829b08a6460d28ce7cd316a9588ddaa3973
Azim Mazinani <mazinani@tutanota.com>
2019-10-15 09:42:46 +0200

    Refactoring.

--- SkyImageAgg/Logger.py
+++ SkyImageAgg/Logger.py
@@ -10,14 +10,29 @@ class Logger:
         self.name = __name__
         self.logger = logging
 
-    def set_logger(self, log_dir=None, stream=True, file_suffix='%Y-%m-%d', level=logging.INFO):
+    def set_logger(
+            self,
+            log_dir=None,
+            stream=True,
+            file_suffix='%Y-%m-%d',
+            level=logging.INFO
+    ):
         handlers = []
         if not log_dir and not stream:
             raise ValueError('path and stream cannot be both False!')
 
         if log_dir:
-            log_file = join(log_dir, 'SIA_logs_{}.log'.format(datetime.utcnow().strftime(file_suffix)))
-            file_handler = TimedRotatingFileHandler(log_file, when='midnight', backupCount=20)
+            log_file = join(
+                log_dir,
+                'SIA_logs_{}.log'.format(datetime.utcnow().strftime(file_suffix))
+            )
+
+            file_handler = TimedRotatingFileHandler(
+                log_file,
+                when='midnight',
+                backupCount=20
+            )
+
             file_handler.suffix = file_suffix
             handlers.append(file_handler)
 
