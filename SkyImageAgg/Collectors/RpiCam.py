2a06b2019ee2130d1149a1ac7375fd5774ec67de
Azim Mazinani <mazinani@tutanota.com>
2020-03-08 14:27:16 +0100

    Added docstrings.

--- SkyImageAgg/Collectors/RpiCam.py
+++ SkyImageAgg/Collectors/RpiCam.py
@@ -11,29 +11,41 @@ from SkyImageAgg.Collectors.Camera import Cam
 
 class RpiCam(Cam):
     """
+    Class for constructing a camera object for Raspberry pi camera.
 
+    Attributes
+    ----------
+    cam : PiCamera
+        RPi camera object.
     """
     def __init__(self):
         super().__init__()
         try:
             self.cam = PiCamera()
-        except picamera.exc.PiCameraMMALError as e:
+        except picamera.exc.PiCameraMMALError:
             raise ConnectionError('It seems that the RPi camera is being used by another application!')
         self.cam.resolution = (2592, 1944)
         self.cam.start_preview()
         time.sleep(2)
 
-    # todo check
+    def login(self, address, username, pwd):
+        """
+        Not implemented.
+        """
+        raise NotImplementedError('This method is not implemented yet!')
+
     def cap_pic(self, output='array'):
         """
+        Capture a picture.
 
         Parameters
         ----------
-        output
-
+        output : str, default 'array'
+            output type of the picture, if a path given, the picture will be saved there.
         Returns
         -------
-
+        numpy.array
+            image array.
         """
         if output == 'array':
             output = BytesIO()
@@ -46,9 +58,6 @@ class RpiCam(Cam):
 
     def cap_video(self, output):
         """
-
-        Parameters
-        ----------
-        output
+        Capture video.
         """
-        raise NotImplementedError
+        raise NotImplementedError('This method is not implemented yet!')
