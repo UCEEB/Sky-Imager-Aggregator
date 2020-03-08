543c4af2d294ba1cc6119736cf1a8849db8c4e60
Azim Mazinani <mazinani@tutanota.com>
2020-03-08 17:11:08 +0100

    Updated docstrings.
    Moving has_internet function to utils.

--- SkyImageAgg/GSM.py
+++ SkyImageAgg/GSM.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import socket
 import time
 from logging import NullHandler
 
@@ -8,23 +7,36 @@ import RPi.GPIO as GPIO
 import serial
 
 from SkyImageAgg import Utils
-
-
-def has_internet(self, host="8.8.8.8", port=53, timeout=20):
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except Exception:
-        return False
+from SkyImageAgg.Utils import has_internet
 
 
 class Modem:
     """
+    Class for communicating with the GPRS/GSM module.
 
+    Attributes
+    ----------
+    port : str
+        the port that the module is connected to.
+    pin : int
+        the GPIO pin that the module is connected to.
+    serial_com : serial.Serial or None, default None
+        the serial object.
     """
 
     def __init__(self, port='/dev/ttyS0', pin=7, logger=None):
+        """
+        Construct a modem object.
+
+        Parameters
+        ----------
+        port : str, default '/dev/ttyS0'
+            the port that the module is connected to.
+        pin : int, default 7
+            the GPIO pin that the module is connected to.
+        logger : logging or None, default None
+            the logging object. if None, a null logging handler will be added.
+        """
         self.port = port
         self.pin = pin
         self.serial_com = None
@@ -35,14 +47,11 @@ class Modem:
         else:
             self._logger = logger
 
-    def set_pin(self, warnings=False):
+    def set_pin(self):
         """
-
-        Parameters
-        ----------
-        warnings
+        Set the GPIO pin.
         """
-        GPIO.setwarnings(warnings)
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
         GPIO.output(self.pin, GPIO.LOW)
@@ -51,7 +60,7 @@ class Modem:
 
     def turn_on_modem(self):
         """
-
+        Turn on the GSM modem.
         """
         if not self.is_power_on():
             self._logger.debug("Switching modem on...")
@@ -63,7 +72,7 @@ class Modem:
 
     def turn_off_modem(self):
         """
-
+        Turn off the GSM modem.
         """
         if self.is_power_on():
             self._logger.debug("Switching modem off...")
@@ -74,19 +83,14 @@ class Modem:
         else:
             self._logger.debug("GSM modem is already OFF...")
 
-    def enable_serial_port(self, port, baudrate=115200, timeout=1):
+    def enable_serial_port(self):
         """
-
-        Parameters
-        ----------
-        port
-        baudrate
-        timeout
+        Open serial port for communication with the modem.
         """
         if not self.serial_com:
             try:
-                self._logger.info('Enabling serial port {} with baudrate {}'.format(port, baudrate))
-                self.serial_com = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+                self._logger.info('Enabling serial port {} with baudrate {}'.format(self.port, 115200))
+                self.serial_com = serial.Serial(self.port, baudrate=115200, timeout=1)
             except Exception as e:
                 self._logger.exception('Serial port error: {}'.format(e))
         else:
@@ -96,10 +100,12 @@ class Modem:
     @Utils.timeout(seconds=15, timeout_exception=TimeoutError, use_signals=False)
     def is_power_on(self):
         """
+        Check if the modem is on or off.
 
         Returns
         -------
-
+        bool
+            if on return True, otherwise False.
         """
         self._logger.debug('Getting modem state...')
         self.enable_serial_port(self.port)
@@ -116,10 +122,12 @@ class Modem:
 
     def send_command(self, command):
         """
+        Send a command to the device.
 
         Parameters
         ----------
-        command
+        command : str
+            command to be sent to the device.
         """
         self.enable_serial_port(self.port)
         time.sleep(.2)
@@ -129,25 +137,41 @@ class Modem:
     @Utils.retry_on_exception(attempts=3, delay=3)
     def force_switch_on(self):
         """
-
+        Try to switch on the modem persistently.
         """
         self.turn_on_modem()
 
 
 class Messenger(Modem):
     """
-
+    A class inherited from Modem class to send sms.
     """
+
     def __init__(self, port='/dev/ttyS0', pin=7, logger=None):
+        """
+        Construct a messenger object.
+
+        Parameters
+        ----------
+        port : str, default '/dev/ttyS0'
+            the port that the module is connected to.
+        pin : int, default 7
+            the GPIO pin that the module is connected to.
+        logger : logging or None, default None
+            the logging object. if None, a null logging handler will be added.
+        """
         super().__init__(port=port, pin=pin, logger=logger)
 
     def send_sms(self, phone_num, sms_text):
         """
+        Send sms.
 
         Parameters
         ----------
-        phone_num
-        sms_text
+        phone_num : int
+            phone number to receive sms.
+        sms_text : str
+            sms text to be sent.
         """
         # transmitting AT command
         self.send_command('AT')
@@ -169,18 +193,39 @@ class Messenger(Modem):
             except Exception as e:
                 self._logger.exception(e)
 
+
 class GPRS(Modem):
     """
+    A class inherited from Modem class to connect to the internet.
 
+    Attributes
+    ----------
+    ppp_config_file : str
+            path to ppp config file based on the service provider.
     """
+
     def __init__(self, ppp_config_file, port='/dev/ttyS0', pin=7, logger=None):
+        """
+        Construct a GPRS object.
+
+        Parameters
+        ----------
+        ppp_config_file : str
+            path to ppp config file based on the service provider.
+        port : str, default '/dev/ttyS0'
+            the port that the module is connected to.
+        pin : int, default 7
+            the GPIO pin that the module is connected to.
+        logger : logging or None, default None
+            the logging object. if None, a null logging handler will be added.
+        """
         super().__init__(port=port, pin=pin, logger=logger)
         self.ppp_config_file = ppp_config_file
 
     @Utils.timeout(seconds=420, timeout_exception=TimeoutError, use_signals=False)
     def check_internet_connection(self):
         """
-
+        Check internet connection persistently.
         """
         while not has_internet():
             time.sleep(5)
@@ -189,7 +234,7 @@ class GPRS(Modem):
     @Utils.retry_on_exception(attempts=3, delay=120)
     def enable_gprs(self):
         """
-
+        Try to enable the GPRS service persistently.
         """
         if not has_internet():
             try:
@@ -210,20 +255,8 @@ class GPRS(Modem):
 
     def disable_gprs(self):
         """
-
+        Disable GPRS service.
         """
         self._logger.debug('disabling ppp')
         os.system('sudo killall pppd > null')
         time.sleep(3)
-
-    @staticmethod
-    def has_ppp():
-        """
-
-        Returns
-        -------
-
-        """
-        if os.system('ps -A | grep pppd > null') == 0:
-            return True
-        return False
